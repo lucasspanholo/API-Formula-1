39cc5ed35529727acf5b2c0764d442b8541cb910
lucas.spanholo <lucasspanholo@gmail.com>
2025-08-28 23:39:24 -0300

    fix: update missing files

--- DTOs/DriverDTO.cs
+++ DTOs/DriverDTO.cs
@@ -1,6 +1,26 @@
+using System.Text.Json.Serialization;
+
 namespace API_Formula1.DTOs;
 
-public class DriverDTO
+public class DriverDTO : BaseDTO
 {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Nationality { get; set; }
+    public int Number { get; set; }
+    public int Points { get; set; }
     
+    public class F1DriversApiResponse
+    {
+        [JsonPropertyName("drivers")]
+        public List<DriverDTO> Drivers { get; set; } = new List<DriverDTO>();
+    }
+
+    public class DriversResponseDto
+    {
+        public int Season { get; set; }
+        public int TotalDrivers { get; set; }
+        public List<DriverDTO> Drivers { get; set; } = new List<DriverDTO>();
+        public DateTime RetrievedAt { get; set; }
+    }
 }
